9e6bc5e52ac087e0b2f2cbec45f07f1aebf43d88
Muhammed Olcay TERCANLI <m.olcay25@gmail.com>
2019-08-06 09:46:37 +0300

    Initial commit

---
+++ pyctx/tests/test_context.py
@@ -0,0 +1,33 @@
+import json
+from unittest import TestCase
+
+from ..context import Context, RequestContext, ContextLog
+
+
+class TestContext(TestCase):
+    def test_context_type(self):
+        ctx = RequestContext({})
+        self.assertTrue(isinstance(ctx, Context))
+        self.assertTrue(isinstance(ctx, RequestContext))
+        self.assertTrue(isinstance(ctx.log, ContextLog))
+
+    def test_context(self):
+        ctx = RequestContext({})
+        ctx.set_http_data({})
+        ctx.set_response({})
+        dict_to_log = ctx.finalize()
+
+        against = {
+            'data': {},
+            'http': {},
+            'type': 'REQ',
+            'ctxId': ctx.context_id,
+            'startTime': ctx.start_time.strftime(ctx.__TIMESTAMP_FORMAT__),
+            'endTime': ctx.end_time.strftime(ctx.__TIMESTAMP_FORMAT__),
+            'timers': {
+                'request': dict_to_log['timers']['request'],
+            }
+        }
+
+        self.assertDictEqual(dict_to_log, against)
+
---
+++ pyctx/tests/test_context_log.py
@@ -0,0 +1,53 @@
+import json
+import time
+from unittest import TestCase
+
+from ..context import ContextLog, Context
+
+
+class TestContext(TestCase):
+    def test_context_log(self):
+        data_against = {'x': 1, 'y': {'z': True}}
+
+        ctx = Context({})
+        log = ContextLog(ctx)
+        log.set_data('x', 1)
+        log.set_data('y/z', True)
+        data, timers = log.finalize()
+
+        self.assertDictEqual(data, data_against)
+        self.assertDictEqual(timers, {})
+
+    def test_context_log_timeit(self):
+        ctx = Context({})
+        log = ContextLog(ctx)
+
+        with log.timeit('timeit_test'):
+            print("before sleep")
+            time.sleep(1)
+            print("after 1 secs")
+
+        _, timers = log.finalize()
+
+        self.assertTrue('timeit_test' in timers)
+        self.assertDictEqual(timers, {'timeit_test': timers['timeit_test']})
+        self.assertGreaterEqual(timers['timeit_test'], 1)
+
+    def test_context_log_timer(self):
+        ctx = Context({})
+        log = ContextLog(ctx)
+
+        log.start_timer('timer1')
+        time.sleep(0.1)
+        log.start_timer('timer2')
+        time.sleep(0.3)
+        log.stop_timer('timer2')
+        time.sleep(0.1)
+        log.stop_timer('timer1')
+
+        _, timers = log.finalize()
+        self.assertTrue('timer1' in timers)
+        self.assertTrue('timer2' in timers)
+        self.assertGreaterEqual(timers['timer1'], 0.5)
+        self.assertGreaterEqual(timers['timer2'], 0.3)
+        self.assertLess(timers['timer2'], 0.5)
